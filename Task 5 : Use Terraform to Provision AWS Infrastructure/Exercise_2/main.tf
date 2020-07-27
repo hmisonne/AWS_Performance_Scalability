4df3622e44cd860e3e15bbf704801cb6ee6751ea
hmisonne <helene.misonne@gmail.com>
2020-07-27 09:18:46 -0700

    docs: Added terraform instructions for project

--- Task 5 : Use Terraform to Provision AWS Infrastructure/Exercise_2/main.tf
+++ Task 5 : Use Terraform to Provision AWS Infrastructure/Exercise_2/main.tf
@@ -38,13 +38,25 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+# package the function for python deployment
+data "archive_file" "app_deployment_file" {
+	type		= "zip"
+	source_file	= "greet_lambda.py"
+	output_path = "app.zip"
+}
+
 
 resource "aws_lambda_function" "lambda_udacity" {
-  filename      = "lambda.zip"
+  filename = "app.zip"
   function_name = "greet_lambda"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "exports.handler"
+  handler       = "greet_lambda.lambda_handler"
   runtime       = "python3.8"
+  environment {
+		variables = {
+			greeting = "Hi there!"
+		}
+	}
 }
 
 resource "aws_cloudwatch_event_rule" "demo_lambda_every_one_minute" {
@@ -69,3 +81,34 @@ resource "aws_lambda_permission" "demo_lambda_every_one_minute" {
   source_arn = "${aws_cloudwatch_event_rule.demo_lambda_every_one_minute.arn}"
 }
 
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],      
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_log_group" "udacity_logs" {
+  name              = aws_lambda_function.lambda_udacity.function_name
+  retention_in_days = 14
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "${aws_iam_policy.lambda_logging.arn}"
+}
--- Task 5 : Use Terraform to Provision AWS Infrastructure/Exercise_2/outputs.tf
+++ Task 5 : Use Terraform to Provision AWS Infrastructure/Exercise_2/outputs.tf
@@ -1,1 +1,6 @@
 # TODO: Define the output variable for the lambda function.
+
+output "lambda_arn"{
+    description = "ARN of Udacity Lambda Function"
+    value = "${aws_lambda_function.lambda_udacity.arn}"
+}
