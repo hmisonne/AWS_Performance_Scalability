92c7d79bacbef16676bcda96dad2b03e48c86ba2
hmisonne <helene.misonne@gmail.com>
2020-07-27 18:45:02 -0700

    docs: Updated exercises

--- Task5/Exercise_1/main.tf
+++ Task5/Exercise_1/main.tf
@@ -10,14 +10,20 @@ resource "aws_instance" "Udacity_T2" {
   count = "4"
   ami = "ami-0c6b1d09930fac512"
   instance_type = "t2.micro"
+  tags = {
+    Name = "Udacity T2"
+  }
 }
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
-# resource "aws_instance" "Udacity_M4" {
-# count = "2"
-# ami = "ami-0c6b1d09930fac512"
-# instance_type = "m4.large"
-#}
+ resource "aws_instance" "Udacity_M4" {
+   count = "2"
+   ami = "ami-0c6b1d09930fac512"
+   instance_type = "m4.large"
+   tags = {
+    Name = "Udacity M4"
+  }
+}
 
 
 
